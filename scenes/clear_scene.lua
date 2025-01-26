c00cb34080bf6ea8983b0f1249f237c8bb03f978
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2025-01-26 14:10:18 +0900

    ending screen

--- scenes/clear_scene.lua
+++ scenes/clear_scene.lua
@@ -1,23 +1,46 @@
 -- clear_scene(nxt)
 -- * nxt : callback - call to end the cutscene and pass control to the next scene
 function clear_scene(nxt)
+
+  local thoughts = {
+    "here! i feel something",
+    "this is it",
+    "the meaning of it all",
+    "is love"
+  }
+  local time_per_thought = 150
+  local thought_index = 0
+
   return {
-    t = 0,
-    text=bubbletext("i did it!",v2(20,60)),
+    time_on_current_thought = 0,
+    text = bubbletext(thoughts[1], v2(10, 10)),
+    bubble = floating_bubble(56, 56, 5, true),
+    current_thought_idx = 1,
+
     init = function(self)
+      music_controller:play_song("liftoff")
     end,
+
     update = function(self)
-      self.t += 1
-      self.text:update()
-      -- Allow early exit with btn press.
-      if btnp(❎) then
-        nxt()
+      self.time_on_current_thought += 1
+      if(self.time_on_current_thought > time_per_thought) then
+        self.time_on_current_thought = 0
+        self.current_thought_idx += 1
+        if self.current_thought_idx > #thoughts then
+          return nxt()
+        end
+        self.text = bubbletext(thoughts[self.current_thought_idx], v2(10, 10))
       end
-      return nil  -- continue
+      self.text:update()
+      self.bubble:update()
+      return nil
     end,
+
     draw = function(self)
-      cls()
+      cls()      
+      self.bubble:draw()
       self.text:draw()
     end,
   }
+
 end
