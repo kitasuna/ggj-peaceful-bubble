fc9cd5da6a64c58c56567ec2ff000eeadfdd776c
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2025-01-26 14:31:43 +0900

    passing ship flame

--- passing_ship.lua
+++ passing_ship.lua
@@ -4,10 +4,15 @@ function passing_ship()
 
     pos = v2((20 + rnd(90)), 150),
     done=false,
+    flame_timer=-15,
 
     update = function(self)
       if self.done then
         return
+      end
+      self.flame_timer += 1
+      if self.flame_timer > 15 then
+        self.flame_timer = -15
       end
       self.pos.y -= speed
       if self.pos.y < -50 then
@@ -20,6 +25,9 @@ function passing_ship()
         return
       end
       spr(198, self.pos.x, self.pos.y, 2, 2)
+      if self.flame_timer > 0 then
+        spr(230, self.pos.x, self.pos.y + 16, 2, 2)
+      end
     end,
 
   }
