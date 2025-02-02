b2e761e8504de5d41fa5b827b5f3785f0ddd5838
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2025-02-02 14:28:48 +0900

    Simplify wrapping bg code

--- wrapping_bg.lua
+++ wrapping_bg.lua
@@ -3,26 +3,21 @@ function wrapping_bg(tx,ty,map_size)
     tx=tx,
     ty=ty,
     offset=v2(0,0),
-    velocity=velocity,
     map_size=map_size,
     scroll=function(self,d)
       self.offset += d
-      if self.offset.x < 0 then
-        self.offset.x += self.map_size*8
-      elseif self.offset.x >= self.map_size*8 then
-        self.offset.x -= self.map_size*8
-      end
-      if self.offset.y < 0 then
-        self.offset.y += self.map_size*8
-      elseif self.offset.y >= self.map_size*8 then
-        self.offset.y -= self.map_size*8
-      end
+      self.offset = v2(
+        self.offset.x % (8*self.map_size),
+        self.offset.y % (8*self.map_size)
+      )
     end,
     draw=function(self)
-      map(self.tx,self.ty,-self.offset.x,-self.offset.y,self.map_size,self.map_size)
-      map(self.tx,self.ty,self.map_size*8-self.offset.x,-self.offset.y,self.map_size,self.map_size)
-      map(self.tx,self.ty,self.map_size*8-self.offset.x,self.map_size*8-self.offset.y,self.map_size,self.map_size)
-      map(self.tx,self.ty,-self.offset.x,self.map_size*8-self.offset.y,self.map_size,self.map_size)
+      local tx,ty,ox,oy = self.tx, self.ty, self.offset.x, self.offset.y
+      local size = self.map_size
+      map(tx,ty,-ox,-oy,size,size)
+      map(tx,ty,8*size-ox,-oy,size,size)
+      map(tx,ty,8*size-ox,8*size-oy,size,size)
+      map(tx,ty,-ox,8*size-oy,size,size)
     end,
   }
 end
