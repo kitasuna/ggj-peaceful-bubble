cad3f9bdbdfd61e58dec69164cbd4f5532a6abf7
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2025-03-02 13:23:48 +0900

    Refactor anims

--- items.lua
+++ items.lua
@@ -1,29 +1,23 @@
 function new_item(start_pos, end_pos)
+  local item_anim = anim.from_frames({39,41,43,45,45,43,41,39})
+    :map(function(s)
+      return draw_spr(s,-8,-8,2,2)
+    end)
+    :scale(8/60)
+    :loop()
+
   return {
     t=0,
     pos=start_pos,
     start_pos=start_pos,
     end_pos=end_pos,
     draw = function(self)
-      spr(self.frames[self.current_frame], self.pos.x-8, self.pos.y-8, 2, 2)
+      draw_with_offset(self.pos)
+        (item_anim.at(self.t))()
     end,
-    frames={39,41,43,45,45,43,41,39},
-    current_frame=1,
-    current_framettl=0.2,
-    -- used to reset the ttl after frame change
-    framettl=0.2,
     update = function(self, dt)
-      self.t += 1
-      self.pos = sine_ease(self.start_pos, self.end_pos, self.t/400)
-
-      self.current_framettl -= dt
-      if self.current_framettl <= 0 then
-        self.current_framettl = self.framettl
-        self.current_frame += 1 
-        if self.current_frame > #self.frames then
-          self.current_frame = 1
-        end
-      end
+      self.t += dt
+      self.pos = sine_ease(self.start_pos, self.end_pos, 60*self.t/400)
     end,
     collider = function(self)
       return circle_collider(self.pos, 7)
@@ -33,12 +27,10 @@ end
 
 function items(phase)
   local is = {
-    -- TODO: Do this math xD The end_pos are calculated from the old
-    -- velocity-based code + how many frames it ran.
-    new_item(v2(136,136), v2(136,136) + v2(-0.25,-0.30)*180),
-    new_item(v2(136,-8), v2(136, -8) + v2(-0.30,0.20)*180),
-    new_item(v2(-8,-8), v2(-8,-8) + v2(0.55,0.35)*120),
-    new_item(v2(136,136), v2(136,136) + v2(-0.25,-0.30)*180),
+    new_item(v2(136,136), v2(91,82)),
+    new_item(v2(136,-8), v2(82, 28)),
+    new_item(v2(-8,-8), v2(58,34)),
+    new_item(v2(136,136), v2(91,82)),
   }
   return is[phase]
 end
--- pilot.lua
+++ pilot.lua
@@ -1,4 +1,8 @@
 function new_pilot(pos)
+  local wobble_anim = anim.from_frames({0,1,2,3,2,1,0,-1})
+    :scale(16/60)
+    :loop()
+
   return {
     t=0,
     pos=pos,
@@ -6,28 +10,18 @@ function new_pilot(pos)
     end_pos=pos+v2(0,60),
     draw = function(self)
       -- draw ship
-      draw_with_outline(0)
-        (draw_spr(198, self.pos.x-24, self.pos.y+8, 2, 2))()
+      compose(
+        draw_with_offset(self.pos),
+        draw_with_outline(0)
+      )(draw_spr(198, -24, 8, 2, 2))()
       -- draw pilot
-      spr(200, self.pos.x-2, self.pos.y-self.offsets[self.current_offset]-2, 1, 1)
+      local dy = wobble_anim.at(self.t)
+      draw_with_offset(self.pos)
+        (draw_spr(200, -2, -dy - 2, 1, 1))()
     end,
-    offsets={0,1,2,3,2,1,0,-1},
-    current_offset=1,
-    current_offsetttl=0.1,
-    -- used to reset the ttl after pos change
-    offsetttl=0.8,
     update = function(self, dt)
-      self.t += 1
-      self.pos = sine_ease(self.start_pos, self.end_pos, self.t/400)
-
-      self.current_offsetttl -= dt
-      if self.current_offsetttl <= 0 then
-        self.current_offsetttl = self.offsetttl
-        self.current_offset += 1 
-        if self.current_offset > #self.offsets then
-          self.current_offset = 1
-        end
-      end
+      self.t += dt
+      self.pos = sine_ease(self.start_pos, self.end_pos, 60*self.t/400)
     end,
     collider=function(self)
       return circle_collider(self.pos, 3)
